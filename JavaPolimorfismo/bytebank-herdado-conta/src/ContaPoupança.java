9621d5a5feb9c7ade07fa673150f896edc8c684e
Cauê Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-03-31 17:13:49 -0300

    Classes e Métodos Abstratos

--- JavaPolimorfismo/bytebank-herdado-conta/src/ContaPoupança.java
+++
@@ -1,8 +0,0 @@
-
-public class ContaPoupança extends Conta {
-
-	
-	public ContaPoupança(int agencia, int numero) {
-		super(agencia, numero);
-	}
-}
--- JavaPolimorfismo/bytebank-herdado/src/EditorVideo.java
+++
@@ -1,9 +0,0 @@
-
-public class EditorVideo extends Funcionario {
-
-	public double getBonificacao() {
-		return super.getBonificacao() + 100; //super faz uma referencia à classe superior, no caso, funcionario
-	}
-	
-	
-}
