ab50b815e696fd03bd47b8581271fe6507da1443
Cauê Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-04-02 10:23:56 -0300

    Aula Exceções e Try Catch

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -1,9 +1,16 @@
-
 public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        
+        try {
+        	metodo1();
+        } catch(ArithmeticException ex) {
+        	//String msg = ex.getMessage();
+        	//System.out.println("ArithmeticException: " + msg);
+        	ex.printStackTrace();
+        }
+        
         System.out.println("Fim do main");
     }
 
@@ -17,6 +24,7 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+            int a = i/0;
         }
         System.out.println("Fim do metodo2");
     }
