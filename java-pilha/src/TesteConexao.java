0340601b6502a5d0d931997350461c59bc50b3f8
Cauê Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-04-07 14:15:55 -0300

    Praticando try-catch com resources

---
+++ java-pilha/src/TesteConexao.java
@@ -0,0 +1,28 @@
+
+public class TesteConexao {
+
+	public static void main(String[] args) {
+		
+		try (Conexao conexao = new Conexao()) {
+			conexao.leDados();
+		} catch(IllegalStateException ex) {
+			System.out.println("Deu erro na conexao");
+		}
+		
+		//---------------------------------------------------------
+		
+		
+//		Conexao con = null;
+//		try {			
+//			con.leDados();
+//			con.leDados();
+//		} catch (IllegalStateException ex) {
+//			System.out.println("Deu erro de conexao!");
+//		} finally {
+//			System.out.println("Finally");
+//			if(con != null) {
+//				con.close();
+//			}
+//		}
+	}
+}
