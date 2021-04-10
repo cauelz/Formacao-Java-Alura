a71ff469100b41c7734f23fa0967f242c6ae83c9
Cauê Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-04-10 12:28:32 -0300

    Apresentando alguns metodos da classe String

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -4,8 +4,8 @@ public class TesteString {
 
 	public static void main(String[] args) {
 		
-		String nome = "Alura"; //object literal
 		// String nome = new String("Alura");
+		String nome = "Alura"; //object literal
 		
 		/**
 		 * Veja que as modificações não foram aplicadas no valor "Alura", mas pq?
@@ -25,10 +25,30 @@ public class TesteString {
 		 * Se um novo valor é RETORNADO, precisamos armazenar em uma outra variavel
 		 * Veja abaixo
 		 */
-		
 		String outroNome = nome.replace("r", "l");
 		System.out.println("Com o método replace: " + outroNome);
+		
+		char c = 'A';
+		char d = 'a';
+		
+		String outroNome2 = nome.replace(c, d);
+		System.out.println("Usando metodo replace com caracteres 'char': " + outroNome2);
 	
+		
+		/**
+		 * Outros métodos:
+		 * 
+		 * char c = nome.charAt(0); retorna o primeiro caracter A
+		 * 
+		 * int c = nome.indexOf("ur"); retorna a posição 2 desta substring
+		 * 
+		 * String sub = nome.substring(1); retorna "lura", ou seja, a posição 1 de uma string
+		 */
+		
+		String vazio = "  ";
+		String outroVazio = vazio.trim();
+		System.out.println(vazio.isEmpty());
+		System.out.println(outroVazio.isEmpty());
 	}
 
 }
