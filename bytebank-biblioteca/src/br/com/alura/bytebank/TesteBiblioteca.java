9cb6bcccd12106f6aa73f512f7ba7a7b204253e3
Cauê Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-04-10 10:48:43 -0300

    Como criar um arquivo JAR

---
+++ bytebank-biblioteca/src/br/com/alura/bytebank/TesteBiblioteca.java
@@ -0,0 +1,16 @@
+package br.com.alura.bytebank;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class TesteBiblioteca {
+
+	public static void main(String[] args) {
+		
+		Conta c = new ContaCorrente(123, 321);
+		c.deposita(300.00);
+		
+		System.out.println(c.getSaldo());
+	}
+
+}
