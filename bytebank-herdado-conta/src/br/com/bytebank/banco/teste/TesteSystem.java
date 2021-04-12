ed4bb0dad47fee45d0ff7b04f289ffb0c1a9745e
Cauê Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-04-12 11:09:13 -0300

    Analisando sysout e os modificadores

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteSystem.java
@@ -0,0 +1,29 @@
+package br.com.bytebank.banco.teste;
+
+/**
+ * 
+ * @author caue zaratin
+ * 
+ * Análise do codigo System.out.println()
+ * 
+ * System:
+ * - classe, java.lang, acesso public
+ * 
+ * ou:
+ * - atributo
+ * - publico
+ * - referencia
+ * - static
+ * 
+ * println:
+ * - metodo
+ * - publico
+ * - nao static
+ * - sobrecarga
+ * - não joga excecoes
+ *
+ */
+
+public class TesteSystem {
+
+}
