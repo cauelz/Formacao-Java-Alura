77224634ade996dccd6f6586d76806f7cdfd2167
Cauê Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-04-26 07:42:01 -0300

    Aula Métodos da classe Integer

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/Test.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/Test.java
@@ -11,12 +11,37 @@ public class Test {
 		String[] nomes = new String[5];
 		
 		int idade = 29; //Integer
-		Integer idadeRef = new Integer(29);
+		
+		/* Não inicializamos um Integer da forma abaixo... 
+		 * 
+		 * Integer idadeRef = new Integer(29);
+		 * */
+		Integer idadeRef = Integer.valueOf(29);
+		int valor = idadeRef.intValue();
+		
+		// Exemplo utilizando o args do metodo main
+		String s1 = args[0];
+		String s2 = args[1];
+		String s3 = args[2];
+		
+		Integer numero1 = Integer.valueOf(s1);
+		int numero2 = Integer.parseInt(s2);
+		double numero3 = Double.parseDouble(s3);
+		
+		System.out.println(numero1);
+		System.out.println(numero2);
+		System.out.println(numero3);
 		
 		List<Integer> numeros = new ArrayList<Integer>();
 		numeros.add(idadeRef);
 		// ou 
 		numeros.add(20); // Autoboxing
 		
+		/**
+		 * Para cada tipo primitivo, existe uma versão em forma de classe.
+		 * Assim podemos trabalhar com referencias.
+		 * 
+		 * java.lang.Integer
+		 */
 	}
 }
