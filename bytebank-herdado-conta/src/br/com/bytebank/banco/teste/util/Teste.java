591dae54c7e1d35373c23b3f8f2d829f516284be
Cauê Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-06-03 18:12:52 -0300

    Aula Lambdas

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/Teste.java
@@ -46,28 +46,16 @@ public class Teste {
 
         // Classe anonima, ou seja, nao tem nome
         lista.sort(
-        	new Comparator<Conta>() {
-	        	@Override
-	        	public int compare(Conta c1, Conta c2) {
-	        		
-	        		return Integer.compare(c1.getNumero(), c2.getNumero());
-	        		
-	        	}
-        	
-        	}
+        	(Conta c1, Conta c2) -> Integer.compare(c1.getNumero(), c2.getNumero())
         );
         
-        Comparator<Conta> comp = new Comparator<Conta>() {
-        	
-        	@Override
-        	public int compare(Conta c1, Conta c2) {
-        		
+        Comparator<Conta> comp = (Conta c1, Conta c2) -> {
         		String nomeC1 = c1.getTitular().getNome();
         		String nomeC2 = c2.getTitular().getNome();
-        		
         		return nomeC1.compareTo(nomeC2);
-        	}
         };
+        
+        lista.forEach( (conta) -> System.out.println(conta + ", " + conta.getTitular().getNome()));
         
         for (Conta conta : lista) {
 			System.out.println(conta);
