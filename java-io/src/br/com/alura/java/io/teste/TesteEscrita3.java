db9f24c57e72799307bbf59097edca892ca96c6d
Cauê Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-06-09 19:58:38 -0300

    Aula de PrintWriter, PrintStream e FileWriter

---
+++ java-io/src/br/com/alura/java/io/teste/TesteEscrita3.java
@@ -0,0 +1,28 @@
+package br.com.alura.java.io.teste;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TesteEscrita3 {
+
+	public static void main(String[] args) throws IOException {
+
+//		//fLUXO DE ENTRADA COM ARQUIVO
+//		OutputStream fos = new FileOutputStream("lorem2.txt");
+//		Writer osw = new OutputStreamWriter(fos);
+//		BufferedWriter bw = new BufferedWriter(osw);
+		
+		// PrintStream ps = new PrintStream("lorem2.txt");
+		PrintWriter ps = new PrintWriter("lorem2.txt");
+		
+		ps.write("texto maluco que está sendo escrito");
+		ps.println();
+		ps.println();
+		ps.println();
+		ps.write("Safados e loucos");
+		
+		ps.close();
+		
+	}
+
+}
